b6f9bba938e74a30a7fb2db65b7b5f4f6662ad8c
Antoine Droyer <antoine.droyer@gmail.com>
2023-10-20 16:29:42 +0200

    feat(front) : display homeworks

---
+++ front/src/components/Devoir.jsx
@@ -0,0 +1,122 @@
+// Devoir.js
+import React, { useState } from 'react';
+
+function Devoir({ nom, lien }) {
+  const [note, setNote] = useState('');
+  const [commentaire, setCommentaire] = useState('');
+  const [isEditing, setIsEditing] = useState(true);
+
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  };
+
+  const handleCommentaireChange = (e) => {
+    setCommentaire(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Note: ${note}, Commentaire: ${commentaire}`);
+    setIsEditing(false);
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEditing(true);
+  }
+
+  const styles = {
+    devoir: {
+      border: '1px solid #e0e0e0',
+      borderRadius: '8px',
+      padding: '16px',
+      margin: '16px 0',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      marginRight: '16px',
+      minHeight : '400px',
+      display: 'flex',
+      flexDirection: 'column',
+      minWidth : '211px',
+    },
+    title: {
+      fontSize: '20px',
+      marginBottom: '12px'
+    },
+    link: {
+      color: '#007BFF',
+      textDecoration: 'none',
+      marginBottom: '16px',
+      display: 'block'
+    },
+    form: {
+      marginTop: '16px'
+    },
+    label: {
+      display: 'block',
+      marginBottom: '8px'
+    },
+    input: {
+      padding: '8px',
+      width: '90%',
+      marginBottom: '12px',
+      borderRadius: '4px',
+      border: '1px solid #ccc',
+      marginRight: '16px'
+    },
+    textarea: {
+      width: '90%',
+      padding: '8px',
+      borderRadius: '4px',
+      border: '1px solid #ccc',
+      minHeight: '80px',
+      marginBottom: '12px',
+      marginRight: '16px'
+    },
+    button: {
+      padding: '8px 16px',
+      backgroundColor: '#007BFF',
+      color: 'white',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer'
+    },
+    content: {
+      marginTop: '16px',
+      marginBottom: '12px',
+      marginRight: '16px'
+    },
+    
+  };
+
+  if (isEditing) {
+    return (
+      <div style={styles.devoir}>
+        <h2 style={styles.title}>{nom}</h2>
+        <a href={lien} target="_blank" rel="noopener noreferrer" style={styles.link}>Voir le devoir</a>
+        <form onSubmit={handleSubmit} style={styles.form}>
+          <div>
+            <label style={styles.label}>Note: </label>
+            <input type="text" value={note} onChange={handleNoteChange} placeholder="Entrez une note" style={styles.input} />
+          </div>
+          <div>
+            <label style={styles.label}>Commentaire: </label>
+            <textarea value={commentaire} onChange={handleCommentaireChange} placeholder="Entrez un commentaire" style={styles.textarea}></textarea>
+          </div>
+          <button type="submit" style={styles.button}>Soumettre</button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div style={styles.devoir}>
+        <h2 style={styles.title}>{nom}</h2>
+        <a href={lien} target="_blank" rel="noopener noreferrer" style={styles.link}>Voir le devoir</a>
+        <div style={styles.content}>Note: {note}</div>
+        <div style={styles.content}>Commentaire: {commentaire}</div>
+        <button onClick={handleEdit} style={styles.button}>Modifier</button>
+      </div>
+    );
+  }
+}
+
+export default Devoir;
